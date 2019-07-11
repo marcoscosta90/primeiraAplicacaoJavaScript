31c0a41be2e21d5e14d76c77642ae0c4d35edf1b
unknown <marcoscostaengproducao@gmail.com>
2019-07-11 11:55:12 -0300

    atualizando

--- js/principal.js
+++ js/principal.js
@@ -9,5 +9,24 @@ var peso = tdPeso.textContent;
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
-var imc = peso / (altura * altura);
-console.log(imc);
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoValido = true;
+var alturaValida = true;
+
+if(peso < 0 || peso > 1000) {
+  console.log("Peso Invalido");
+  pesoValido = false;
+  tdImc.textContent = "Peso Invalido";
+}
+
+if(altura < 0 || altura > 3) {
+  console.log("Altura Invalida");
+  alturaValida = false;
+  tdImc.textContent = "Altura Invalida";
+}
+
+if(pesoValido && alturaValida){
+  var imc = peso / (altura * altura);
+  tdImc.textContent = imc;
+}
